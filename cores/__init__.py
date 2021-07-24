777ab80d3e03adfa3e10c8a8556f8533929fd383
Nong Hoang Tu <dmknght@parrotsec.org>
2021-07-24 11:58:41 +0700

    Add more docker repo

--- cores/__init__.py
+++ cores/__init__.py
@@ -1,6 +1,20 @@
 lab_images = [
-    ("bWAPP", "Owasp's bWAPP", "raesene/bwapp"),
-    ("phpLdapAdmin", "phpLdapAdmin multiple vulns", "vulnerables/phpldapadmin-remote-dump")
+    ("phpLdapAdmin", "phpLdapAdmin multiple vulns", "vulnerables/phpldapadmin-remote-dump"),
+    ("DVWA", "PHP/MySQL vulnerable web app. Default cred: admin/password", "vulnerables/web-dvwa"),
+    ("RailsGoat", "Vulnerable WebApp in Ruby on Rails Framework", "vulnerables/web-owasp-railsgoat"),
+    ("SambaCry", "SambaCry RCE vulnerability (CVE-2017-7494 Samba 4.5.9)"),
+    ("HarakaMail", "Haraka mail 2.8.9 RCE", "vulnerables/mail-haraka-2.8.9-rce"),
+    ("Shellshock", "CVE 2014-6271 Bashdoor", "vulnerables/cve-2014-6271"),
+    ("Heartbleed", "CVE-2014-0160 OpenSSL Data leak", "vulnerables/cve-2014-0160"),
+    ("SpringSecurity Oauth", "CVE-2016-4977 Spring Security Oauth RCE", "vulnerables/cve-2016-4977"),
+    ("OpenSSHDenialofService", "CVE-2016-6515 OpenSSH before 7.3 CPU consumption", "vulnerables/cve-2016-6515"),
+    ("RoundcubeRCE", "Roundcube WebMail server 1.0.0 - 1.2.2 RCE", "vulnerables/web-roundcube-1.2.2-rce"),
+    ("WebGoatPHP", "Vulnerable PHP Web App", "vulnerables/web-owasp-phpgoat"),
+    ("NodeGoat", "Vulnerable NodeJS Web App", "vulnerables/web-owasp-nodegoat"),
+    ("Mutillidae2", "OWASP's Mutillidae 2 vulnerable Web app", "vulnerables/web-owasp-mutillidae2"),
+    ("bWAPP", "Vulnerable PHP Web App", "vulnerables/web-bwapp"),
+    ("BrokenWebApp", "OWASP Broken Web Applications", "vulnerables/web-owasp"),
+    ("PHPMailer", "PHPMailer < 5.2.18 RCE", "vulnerables/cve-2016-10033"),
+    ("ApacheStruts2", "CVE-2017-5638 Apache Struts 2 File Upload", "jrrdev/cve-2017-5638"),
+    ("OracleWebLogic", "CVE-2017-10271 Oracle WebLogic 10.3.6.0.0 WLS-WSAT Deserialization", "kkirsche/cve-2017-10271")
 ]
-
-# TODO check if docker service is running
